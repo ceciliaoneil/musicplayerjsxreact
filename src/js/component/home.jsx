68b214ab6031974d5efe9b970ecfb1dd6074e4cd
ceciliaoneil <ceciliaoneil.di@gmail.com>
2022-10-10 23:54:55 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,87 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect, useRef } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [lista, setLista] = useState([]);
+  const [play, setPlay] = useState(null);
+  const [pocisionMusica, setPocisionMusica] = useState(0)
+  const etiquetaAudio = useRef(null);
+  console.log(pocisionMusica);
+
+	// fecth
+  function getInfo() {
+    fetch("https://assets.breatheco.de/apis/sound/songs") //ir a busca
+      .then((response) => {
+        console.log(response.status);
+        return response.json();
+      }) //promesa 1
+      .then((data) => setLista(data)) //promesa 2
+      .catch((err) => console.log(err));
+	}
+	console.log(lista);
+
+	// Use effect
+	useEffect(() => {
+		getInfo();
+	}, []);
+	console.log(lista);
+
+	const setSong = (linkName, i) => {
+		etiquetaAudio.current.src = `https://assets.breatheco.de/apis/sound/${linkName}`
+    etiquetaAudio.current.play();
+		setPlay(i)
+	}
+
+	const playMusic = () => {
+	if (etiquetaAudio !== null) {
+		etiquetaAudio.current.play();
+	}
+}
+
+	const pauseMusic = () => {
+	if (etiquetaAudio !== play ) {
+		etiquetaAudio.current.pause();
+	}
+}
+
+const nextMusic = () => {
+  if (pocisionMusica <= lista.length -1) {
+    setPocisionMusica(pocisionMusica+1);
+  } else { setPocisionMusica(0);}
+  etiquetaAudio.current.src = `https://assets.breatheco.de/apis/sound/${lista[pocisionMusica].url}`
+    etiquetaAudio.current.play();
+    console.log(lista[pocisionMusica].url);
+}
+
+console.log(pocisionMusica);
+
+const anteriorMusic = () => {
+	if (pocisionMusica > 0) {
+  setPocisionMusica(pocisionMusica-1);
+} else { setPocisionMusica(lista.length -1);}
+etiquetaAudio.current.src = `https://assets.breatheco.de/apis/sound/${lista[pocisionMusica].url}`
+  etiquetaAudio.current.play();
+  console.log(lista[pocisionMusica].url);
+}
+
+  return (
+    <div className="text-center">
+      <p></p>
+      {/* <a href="#" className="btn btn-success" onClick={()=>setCount(+1)}>
+				Follow                                       {play === index}
+			</a> */}
+      <ol>                                                        
+		{/* {lista} */}
+        {lista.map((item, index) => (<li key={index} className="btn btn-danger d-flex flex-column mb-3" onClick={()=>setSong(item.url,index)}>{item.name}</li>))}</ol>
+      <div className="container">
+	 	<audio controls ref={etiquetaAudio} src="https://assets.breatheco.de/apis/sound/songs" type="audio.mp3"/><hr />
+        <button onClick={anteriorMusic}><i className="fa fa-backward"/></button>
+        <button onClick={pauseMusic}><i className="fa fa-pause"/></button>
+        <button onClick={playMusic}><i className="fa fa-play"/></button>
+        <button onClick={nextMusic}><i className="fa fa-forward"/></button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
